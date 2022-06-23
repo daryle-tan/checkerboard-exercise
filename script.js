f85017c938dc7b2008d9176ddfab76bb79874c48
Daryle Tan <daryle.tan@gmail.com>
2022-06-23 17:55:45 -0500

    checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your JS goes here
+var container = document.createElement('div');
+document.body.append(container);
+
+function checkerBoard() {
+for(var i=0; i < 63; i++){
+  
+    var div = document.createElement('div');
+    
+    div.style.width = '11.11%';
+    div.style.float = "left";
+    div.style.paddingBottom = '11.1%';
+    
+    // div.style.border = '1px solid black';
+    container.append(div);
+
+    if (i % 2 === 0) {
+        div.style.background = 'red';
+    } else {
+        div.style.background = 'black';
+    }
+}
+}
+
+checkerBoard()
+
