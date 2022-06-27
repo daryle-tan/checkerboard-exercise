a1c7895624596dcada33b26b9950985bca8c5f38
daryle-tan <daryle.tan@gmail.com>
2022-06-26 21:52:58 -0500

    test

--- script.js
+++ script.js
@@ -2,23 +2,29 @@ var container = document.createElement('div');
 document.body.append(container);
 
 function checkerBoard() {
-for(var i=0; i < 63; i++){
-  
+  for(i=0; i < 81; i++) {
+    var x = Math.floor(Math.random() * 256);
+    var y = 100+ Math.floor(Math.random() * 256);
+    var z = 50+ Math.floor(Math.random() * 256);
+    var color = "rgb(" + x + "," + y + "," + z + ")";
     var div = document.createElement('div');
-    
+      
     div.style.width = '11.11%';
     div.style.float = "left";
     div.style.paddingBottom = '11.1%';
     
-    // div.style.border = '1px solid black';
+    
     container.append(div);
-
-    if (i % 2 === 0) {
-        div.style.background = 'red';
-    } else {
-        div.style.background = 'black';
+    
+    if(i % 2 === 0) {
+      div.style.background = `rgb(${x}, ${y}, ${z})`;
+    } else {     
+      div.style.background = `rgb(${x}, ${y}, ${z})`;
     }
-}
+    x += 3;
+    y += 2;
+    z -= 2;
+  }
 }
 
 checkerBoard()
